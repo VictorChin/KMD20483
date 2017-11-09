bf678a0f6c4d046f8d2299be986de96b32eb774d
dev <dev@vchins.com>
2017-11-09 13:35:39 +0100

    Add Crypto Project

--- ClassicADONET/Program.cs
+++ ClassicADONET/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,51 @@ namespace ClassicADONET
     {
         static void Main(string[] args)
         {
+            using (SqlConnection conn = new SqlConnection())
+            { 
+                conn.ConnectionString =
+                    "Server=tcp:kmddemo.database.windows.net,1433;Initial Catalog=kmddb;Persist Security Info=False;User ID=chinzilla;Password=Pa$$w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                conn.Open();
+                string query = "Select * from SalesLT.Product";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                List<ProdPrice> prodList = new List<ProdPrice>();
+                while (reader.Read())
+                {
+                    //Console.WriteLine($"{reader["Name"]} : {reader["ListPrice"]} ");
+                    prodList.Add(new ProdPrice { Name = reader.GetString(1),
+                        Price = reader.GetDecimal(5) });
+                }
+                var q =
+                     from p in prodList
+                     group p by p.Name[0] into InitGroup
+                     orderby InitGroup.Key
+                     select new
+                     {
+                         GroupKey = InitGroup.Key,
+                         NumberOfProduct = InitGroup.Count(),
+                         Products = InitGroup.OrderByDescending(p=>p.Price).Skip(3).Take(5),
+                         GroupPriceTotal = InitGroup.Sum(p=>p.Price)
+                     };
+                
+                foreach (var item in q)
+                {
+                    Console.WriteLine($"Group:{item.GroupKey} " +
+                        $"- TotalPrice:{item.GroupPriceTotal} - " +
+                        $"ProductCount :{item.NumberOfProduct} " );
+                    foreach (var product in item.Products)
+                    {
+                        Console.WriteLine($"\t{product.Name}:{product.Price:C}");
+                    }
+
+                }
+
+            }//conn.Dispose() is called because end of using{}
+        }
+        class ProdPrice
+        {
+            public string Name;
+            public decimal Price;
         }
     }
 }
---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿Create Table SampleTable
+(id int primary key,
+ name varchar(30),
+ phone varchar(10),
+ dob datetime);
+ insert into SampleTable
+ values (1,'Victor','2125551212','19750721');
+ insert into SampleTable
+ values (2,'Bob','2125551213','19730721');
+insert into SampleTable
+ values (3,'Jon','2125551214','19790721');
+
+
+select name,phone from SampleTable where dob > '19750101';
+select min(dob),max(dob) from SampleTable;
+
